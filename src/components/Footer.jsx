50ff5a6b111131eae5e17d156a920b6f17fae2e0
August <augustjp@live.dk>
2024-04-09 11:53:45 +0200

    css in react

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,8 @@
+import styles from "./Footer.modules.css";
+
 function Footer(Props) {
   return (
-    <footer>
+    <footer className={styles.Footer}>
       <p>
         Her er en meget standard footer hvor jeg gerne vil vise {Props.contact}
       </p>
---
+++ src/components/navigation.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function navigation() {
+  const links = ["/", "/about-us", "/stuff"];
+  return <nav>{links}</nav>;
+}
